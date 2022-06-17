81f263a1b1f3a6919f8466d7451952587369e3d6
Bullrun5k <Bullrun5k@gmail.com>
2022-06-17 09:18:03 -0400

    !

--- backend/src/config.js
+++ backend/src/config.js
@@ -50,7 +50,7 @@ const METADATA_UPDATABLE = true; // set to false if you don't want to allow meta
 const OWNER_ADDRESS = '0x9174533Be5c50b88cca8F25a386DcfCa6f45983C';
 const TREASURY_ADDRESS = '0x9174533Be5c50b88cca8F25a386DcfCa6f45983C';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = .0001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = .001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
